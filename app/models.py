0c9af67781076c54858ce37642fe199429f50684
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-28 01:05:18 -0800

    Chapter 8: Password hashing with Passlib (8a)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from passlib.apps import custom_app_context as pwd_context
 from . import db
 
 
@@ -16,6 +17,18 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
     def __repr__(self):
         return '<User %r>' % self.username
